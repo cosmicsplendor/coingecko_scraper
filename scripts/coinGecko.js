cc8a2623a8602aac3cabfc6a0faf769fdffb41b6
Umesh KC <ufocalypse@gmail.com>
2025-05-20 11:41:52 +0545

    Scrape a list of top 120 coins

--- scripts/coinGecko.js
+++ scripts/coinGecko.js
@@ -5,14 +5,21 @@ const apikey = "CG-k6yFVvmqbiDpesiehZnM4APt";
 
 const dir = path.join(__dirname, "../export/coingecko");
 
-axios.get("https://api.coingecko.com/api/v3/coins/list", {
+axios.get("https://api.coingecko.com/api/v3/coins/markets", {
     headers: {
         "x-cg-demo-api-key": apikey
+    },
+    params: {
+        vs_currency: "usd",
+        order: "market_cap_desc",
+        per_page: 120,
+        page: 1,
+        sparkline: false
     }
 }).then(async res => {
     await fs.mkdir(dir, { recursive: true }); // Ensure directory exists
-    await fs.writeFile(path.join(dir, "list.json"), JSON.stringify(res.data, null, 2));
-    console.log("Data written to /export/coingecko/list.json");
+    await fs.writeFile(path.join(dir, "markets.json"), JSON.stringify(res.data, null, 2));
+    console.log("Data written to /export/coingecko/markets.json");
 }).catch(err => {
     console.error(err);
 });
